275df3fadea0c83059fa8babef699b187b217a9f
Mélissa Aiello <maiello.contact@gmail.com>
2021-10-13 13:42:27 +0200

    AJOUT cart.js affichage du panier

--- frontend/script/cart.js
+++ frontend/script/cart.js
@@ -1,4 +1,6 @@
-let produitsPanier = JSON.parse(localStorage.getItem("produitsPanier") || "['Le panier est vide']");
+let produitsPanier = JSON.parse(
+  localStorage.getItem("produitsPanier") || "['Le panier est vide']"
+);
 console.log(produitsPanier);
 
 const showPanier = document.querySelector("#liste-panier");
@@ -9,29 +11,34 @@ function formaterPrix(prix) {
   return arrondi.toFixed(2);
 }
 
-let affichagePanier = '';
+let affichagePanier = "";
 console.log(affichagePanier);
 
 let montantTotal = 0;
 
-for(k = 0; k < produitsPanier.length; k++){
-  montantTotal= montantTotal + produitsPanier[k].prix;
+for (k = 0; k < produitsPanier.length; k++) {
+  montantTotal = montantTotal + produitsPanier[k].prix;
 
-  affichagePanier = affichagePanier + `
+  affichagePanier =
+    affichagePanier +
+    `
   <div class="fw-bold"> ${produitsPanier[k].nom} </div>
-  <div class="font-weight-bold"> Quantité : ${produitsPanier[k].quantite || 1} </div>
-  <div class="text-decoration-underline "> Sous-total : ${formaterPrix(produitsPanier[k].prix)} € </div>
+  <div class="font-weight-bold"> Quantité : ${
+    produitsPanier[k].quantite || 1
+  } </div>
+  <div class="text-decoration-underline "> Sous-total : ${formaterPrix(
+    produitsPanier[k].prix
+  )} € </div>
 `;
 }
-  
-  showPanier.innerHTML = affichagePanier + `<div class="fw-bold"> PRIX TOTAL DU PANIER : ${formaterPrix(montantTotal)}€</div>`;
-  
 
+showPanier.innerHTML =
+  affichagePanier +
+  `<div class="fw-bold"> PRIX TOTAL DU PANIER : ${formaterPrix(
+    montantTotal
+  )}€</div>`;
 
 let produitsDuPanier = JSON.parse(
-    localStorage.getItem("produitsPanier") || "[]"
-  );
-  console.log(produitsDuPanier);
-  
-  
-  
+  localStorage.getItem("produitsPanier") || "[]"
+);
+console.log(produitsDuPanier);
